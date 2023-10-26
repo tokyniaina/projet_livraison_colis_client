44a54fae1dbc1bd5231867e8229dc236691ad1b0
tokyniaina <tokyniaina54@gmail.com>
2023-10-26 07:35:35 +0200

    desacetive axios.js

--- ReactJs_Livraison_colie/projet_livraison/src/composants/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-
-export const dataUser = useEffect(() => {
-    axios.get("http://localhost:4000/api/users")
-      .then((res) => res.json().then(res.data))
-  }, [])
